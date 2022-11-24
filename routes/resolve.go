47d51109aa675283d64fc826558449a3f9ab789c
Rwitesh Bera <rwiteshbera@gmail.com>
2022-11-24 22:08:13 +0530

    added redis client

--- routes/resolve.go
+++ routes/resolve.go
@@ -1,9 +1,34 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
+	"github.com/rwiteshbera/URL-Shortener-Go/database"
 )
 
 func ResolveURL(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/api", func(ctx *gin.Context) {
+		url := ctx.Param("url")
 
+		r := database.CreateClient(0)
+		defer r.Close()
+
+		value, err := r.Get(database.Ctx, url).Result()
+		if err == redis.Nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "short not found"})
+			return
+		} else if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "cannot connect to database"})
+			return
+		}
+
+		incrementCount := database.CreateClient(1)
+		defer incrementCount.Close()
+
+		_ = incrementCount.Incr(database.Ctx, "counter")
+
+		ctx.Redirect(http.StatusMovedPermanently, value)
+	})
 }
--- routes/shorten.go
+++ routes/shorten.go
@@ -2,9 +2,13 @@ package routes
 
 import (
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
+	"github.com/rwiteshbera/URL-Shortener-Go/database"
 	"github.com/rwiteshbera/URL-Shortener-Go/helpers"
 )
 
@@ -23,7 +27,7 @@ type response struct {
 }
 
 func ShortenURL(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("/api", func(ctx *gin.Context) {
+	incomingRoutes.POST("/api/v1", func(ctx *gin.Context) {
 		var req request
 
 		err := ctx.BindJSON(&req)
@@ -35,6 +39,21 @@ func ShortenURL(incomingRoutes *gin.Engine) {
 		}
 
 		// Implement rate limiting
+		r := database.CreateClient(1)
+		defer r.Close()
+
+		value, err := r.Get(database.Ctx, ctx.ClientIP()).Result()
+		if err == redis.Nil {
+			_ = r.Set(database.Ctx, ctx.ClientIP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err() // 30 minutes to expire
+		} else {
+			val, _ := r.Get(database.Ctx, ctx.ClientIP()).Result()
+			valueInt, _ := strconv.Atoi(val)
+
+			if valueInt <= 0 {
+				limit, _ := r.TTL(database.Ctx, ctx.ClientIP()).Result()
+				ctx.JSON(http.StatusServiceUnavailable, gin.H{"error": "Rate limit exceeded", "limit": limit})
+			}
+		}
 
 		// Check if the input is an actual url
 		if !goValidator.isURL(req.URL) {
@@ -48,8 +67,10 @@ func ShortenURL(incomingRoutes *gin.Engine) {
 			return
 		}
 
-		// enfore https, SSl
+		// enfore https, SSL
 		req.URL = helpers.EnforceHTTP(req.URL)
+
+		r.Decr(database.Ctx, ctx.ClientIP())
 
 		ctx.JSON(http.StatusOK, gin.H{"req": req})
 	})
