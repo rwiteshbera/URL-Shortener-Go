6759a202aef92b1b452603946ce1daeb91e47b41
Rwitesh Bera <rwiteshbera@gmail.com>
2022-12-01 11:48:09 +0530

    Added proper comments to understand codebase

--- routes/shorten.go
+++ routes/shorten.go
@@ -14,11 +14,23 @@ import (
 	"github.com/rwiteshbera/URL-Shortener-Go/helpers"
 )
 
+// A request is a struct with two fields, URL and Expiry, where URL is a string and Expiry is a
+// time.Duration.
+// @property {string} URL - The URL of the page to be shortened.
+// @property Expiry - The time duration for which the URL should be stored.
 type request struct {
 	URL    string        `json:"url"`
 	Expiry time.Duration `json:"expiry"`
 }
 
+// `response` is a type that has a `string` field called `URL`, a `string` field called `CustomShort`,
+// a `time.Duration` field called `Expiry`, an `int` field called `XRateRemaining`, and a
+// `time.Duration` field called `XRateLimitReset`.
+// @property {string} URL - The URL that was provided by user.
+// @property {string} CustomShort - The custom short URL.
+// @property Expiry - The time in seconds that the link will be active for.
+// @property {int} XRateRemaining - The number of requests remaining in the current rate limit window.
+// @property XRateLimitReset - The time in seconds until the rate limit resets.
 type response struct {
 	URL             string        `json:"url"`
 	CustomShort     string        `json:"short"`
@@ -27,6 +39,7 @@ type response struct {
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
 
+// It takes a URL as input, checks if it is valid, checks if the domain is valid, checks if the URL is using HTTPS, generates a short URL, and returns the short URL
 func ShortenURL(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/api/v1", func(ctx *gin.Context) {
 		var req request
@@ -38,16 +51,22 @@ func ShortenURL(incomingRoutes *gin.Engine) {
 		}
 
 		// Implement rate limiting
+		// Creating a new redis client.
 		ipDatabase := database.CreateClient(1)
 		defer ipDatabase.Close()
 
+		// Getting the value of the key `ctx.ClientIP()` from the database.
 		value, err := ipDatabase.Get(database.Ctx, ctx.ClientIP()).Result()
 		if err == redis.Nil {
-			_ = ipDatabase.Set(database.Ctx, ctx.ClientIP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err() // 30 minutes to expire
+			// Setting the value of the key `ctx.ClientIP()` to the value of the environment variable
+			// `API_QUOTA` and the time to live of the key `ctx.ClientIP()` to 30 minutes.
+			_ = ipDatabase.Set(database.Ctx, ctx.ClientIP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
 		} else {
+			// Converting the string value of the key `ctx.ClientIP()` to an integer.
 			valueInt, _ := strconv.Atoi(value)
 
 			if valueInt <= 0 {
+				// Getting the time to live of the key `ctx.ClientIP()` from the database.
 				limit, _ := ipDatabase.TTL(database.Ctx, ctx.ClientIP()).Result()
 				ctx.JSON(http.StatusServiceUnavailable, gin.H{"error": "Rate limit exceededs", "limit": limit})
 				return
@@ -67,6 +86,7 @@ func ShortenURL(incomingRoutes *gin.Engine) {
 		}
 
 		// enfore https, SSL
+		// Checking if the URL is using HTTPS or not. If not, it is adding HTTPS to the URL.
 		req.URL = helpers.EnforceHTTP(req.URL)
 
 		// Generate shorten URL
@@ -79,6 +99,7 @@ func ShortenURL(incomingRoutes *gin.Engine) {
 			req.Expiry = 24 // 24 Hours (Default)
 		}
 
+		// Checking if the URL is already present in the database.
 		isPresent, err := urlDatabase.Exists(ctx, req.URL).Result()
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
@@ -105,9 +126,13 @@ func ShortenURL(incomingRoutes *gin.Engine) {
 			XRateLimitReset: 30,
 		}
 
+		// Getting the value of the key `ctx.ClientIP()` from the database.
 		val, _ := ipDatabase.Get(database.Ctx, ctx.ClientIP()).Result()
+
+		// Converting the string value of the key `ctx.ClientIP()` to an integer.
 		resp.XRateRemaining, _ = strconv.Atoi(val)
 
+		// Getting the time to live of the key `ctx.ClientIP()` from the database.
 		ttl, _ := ipDatabase.TTL(database.Ctx, ctx.ClientIP()).Result()
 		resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
 
